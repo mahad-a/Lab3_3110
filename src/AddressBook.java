84bfdb79a8acf02637d5b9d6ab462990c502a711
Mahad Ahmed <mahad.ahmed613@gmail.com>
2023-10-30 08:13:52 -0400

    Git finally starts up, however buttons do not work yet, will be attempting again soon

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,86 +1,151 @@
 import java.awt.*;
 import java.util.ArrayList;
 import javax.swing.*;
-import java.awt.AWTEvent;
-import java.util.*;
 import java.awt.event.*;
+import java.util.Random;
 
 
-public class AddressBook extends BuddyInfo {
-    /**
-     * Create an AddressBook class that contains a
-     * collection of BuddyInfo objects (the choice of
-     * the collection class is entirely up to you). Provide
-     * an addBuddy() and a removeBuddy() method
-     */
-    private ArrayList<BuddyInfo> addressBook;
-    private JMenuItem quitItem;
+public class AddressBook extends DefaultListModel {
+
+    private DefaultListModel<BuddyInfo> addressBook;
+    private JList<BuddyInfo> buddyList;
+    private Random random;
+
+
+    /* public AddressBook() {
+        addressBook = new DefaultListModel<>(); // restarts address book
+    } */
 
     // GUI stuff
-    public TicTacToeGUI()
+    public AddressBook()
     {
-        JFrame frame = new JFrame("Tic Tac Toe");
+        addressBook = new DefaultListModel<>();
+        buddyList = new JList<>(addressBook);
+
+        JFrame frame = new JFrame("Address Book");
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(new BorderLayout());
         frame.setPreferredSize(new Dimension (500, 500));
 
-        JMenuBar menubar = new JMenuBar();
-        frame.setJMenuBar(menubar);
 
-        JMenu fileMenu = new JMenu("Options");
-        menubar.add(fileMenu);
-        fileMenu.addMouseListener(this);
+        JMenuBar menu = new JMenuBar();
 
-        quitItem = new JMenuItem("Quit Game");
-        fileMenu.add(quitItem);
+        JMenu addressBookMenu = new JMenu("AddressBook");
+        JMenu buddyInfoMenu = new JMenu("BuddyInfo");
 
-        quitItem.addActionListener(new ActionListener() // create an anonymous inner class
-                                   { // start of anonymous subclass of ActionListener
-                                       // this allows us to put the code for this action here
-                                       public void actionPerformed(ActionEvent event)
-                                       {
-                                           System.exit(0); // quit
-                                       }
-                                   } // end of anonymous subclass
-        );
 
+        // Menu Items
+        JMenuItem newAddressBook = new JMenuItem("Create New AddressBook");
+        newAddressBook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createNewAddressBook();
+            }
+        });
+
+        JMenuItem addBuddy = new JMenuItem("Add Buddy to AddressBook");
+        newAddressBook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addBuddy();
+            }
+        });
+
+        JMenuItem removeBuddy = new JMenuItem("Remove Buddy from AddressBook");
+        newAddressBook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeBuddy();
+            }
+        });
+
+        JMenuItem displayAddressBook = new JMenuItem("Display Current AddressBook");
+        newAddressBook.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                displayAddressBook();
+            }
+        });
+
+
+
+        addressBookMenu.add(newAddressBook);
+        addressBookMenu.add(displayAddressBook);
+        buddyInfoMenu.add(addBuddy);
+        buddyInfoMenu.add(removeBuddy);
+
+        menu.add(addressBookMenu);
+        menu.add(buddyInfoMenu);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(new JScrollPane(buddyList), BorderLayout.CENTER);
+
+        frame.setJMenuBar(menu);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setResizable(false);
+        //frame.pack();
+        frame.setTitle("Address Book");
+        frame.setResizable(true);
         frame.setVisible(true);
 
+        displayAddressBook();
     }
 
 
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
-        AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(0);
-        // check if changes are being updated
-    }
-    public AddressBook() {
-        addressBook = new ArrayList<>(); // restarts address book
+        SwingUtilities.invokeLater(() -> {
+            AddressBook addressBookMain = new AddressBook();
+        });
     }
 
-    public void addBuddy(BuddyInfo newBuddy) {
-        if (newBuddy != null) {
-            addressBook.add(newBuddy);
+
+    public void addBuddy() {
+        displayAddressBook();
+        String buddyName = JOptionPane.showInputDialog("Please enter the name of the buddy you wish to add");
+        String buddyAddress = JOptionPane.showInputDialog("Please enter the address of the buddy");
+
+        Random random = new Random(); // generate random number for temporary
+        int buddyNumber = random.nextInt(1000);
+
+        if (buddyName != null || buddyAddress != null) { // user types in a name
+            new BuddyInfo(buddyName, buddyAddress, buddyNumber);
         }
+
+        displayAddressBook();
+        System.out.println("Successfully added: " + buddyName);
     }
 
-    public BuddyInfo removeBuddy(int index) {
-        if (index >= 0 && index < addressBook.size()) {
-            return addressBook.remove(index);
+    public void removeBuddy() {
+        displayAddressBook();
+        String buddyName = JOptionPane.showInputDialog("Please enter the name of the buddy you wish to remove");
+
+        if (buddyName != null) { // user types in a name
+            // downsize since JList cannot remove
+            DefaultListModel<BuddyInfo> tempBuddyList = (DefaultListModel<BuddyInfo>) buddyList.getModel();
+
+            for (int i = 0; i < tempBuddyList.size(); i++) {
+                if (tempBuddyList.getElementAt(i).getName().equals(buddyName)) {
+                    addressBook.remove(i);
+                    tempBuddyList.remove(i);
+                    break; // only need to remove the first instance
+                }
+            }
         }
-        return null;
+
+        displayAddressBook();
+        System.out.println("Successfully removed: " + buddyName);
     }
 
     public void displayAddressBook() {
-        for (BuddyInfo buddy: addressBook) {
-            buddy.displayBuddy(buddy);
+        DefaultListModel<BuddyInfo> display = (DefaultListModel<BuddyInfo>) buddyList.getModel();
+
+        for (int i = 0; i < addressBook.size(); i++) {
+            display.addElement(addressBook.getElementAt(i));
         }
+    }
+
+    public void createNewAddressBook() { // wipes current address book
+        addressBook.clear();
     }
 
 
